bba57b58f441b1a7f2f082d31c51573bbb85be9e
BAKO ADAMOU <barikibako@gmail.com>
2022-01-10 14:23:25 +0100

    Modification du fichier livraisonController.java

--- src/main/java/com/aratech/demo/controllers/LivraisonController.java
+++ src/main/java/com/aratech/demo/controllers/LivraisonController.java
@@ -2,12 +2,14 @@ package com.aratech.demo.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aratech.demo.models.Livraison;
@@ -30,4 +32,16 @@ public class LivraisonController {
 		
 		return livraisonRepository.findAll(page);
 	}
+	
+	@GetMapping("/api/mesLivraisons/{id}")
+	public Livraison getLivraison(@PathVariable(value="id") long idLivraison){
+		Livraison l = null;
+		Optional<Livraison> findItem = livraisonRepository.findById(idLivraison);
+		if(findItem.isPresent()) {
+			l = findItem.get();
+		}
+		return l;
+	}
+	
+	
 }
